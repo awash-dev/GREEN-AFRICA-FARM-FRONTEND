8894cdf97e011301065d6c0eb5ef1fa16065433c
awash-dev <mohammedhusse4242@gmail.com>
2025-12-19 23:42:56 +0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/products";
+const API_URL = "https://green-africa-farm-backend.vercel.app/api/products";
 
 export interface Product {
   id?: number;
